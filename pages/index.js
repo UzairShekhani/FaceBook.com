f91abde1bb7a8b2c4be15987e5c91f4a9b111eb7
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-06-15 18:46:02 +0500

    ui updated

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 export default function Home() {
   return (
     <>
-      <h1>let build facebook </h1>
+      <h1> let build facebook </h1>
     </>
   );
 }
